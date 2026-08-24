41cbe493b7e3597f01742b74bd103cb4c60cbc2b
Fernando Luckesi <fernando.luckesi@a5x.com.br>
2026-08-24 15:29:30 -0300

    n

--- src/clinics/dto/create-clinic.dto.ts
+++ src/clinics/dto/create-clinic.dto.ts
@@ -21,15 +21,14 @@ export class CreateClinicDto {
   clinicName: string;
 
   @ApiProperty({
-    example: '12.345.678/0001-99',
-    description: 'CNPJ da clínica (formato XX.XXX.XXX/XXXX-XX)',
+    example: '56770702000109',
+    description: 'CNPJ da clínica (somente números, 14 dígitos)',
     required: false,
   })
   @IsOptional()
   @IsString()
-  @MaxLength(18)
-  @Matches(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/, {
-    message: 'CNPJ deve estar no formato XX.XXX.XXX/XXXX-XX',
+  @Matches(/^\d{14}$/, {
+    message: 'CNPJ deve conter exatamente 14 dígitos numéricos',
   })
   cnpj?: string;
 
